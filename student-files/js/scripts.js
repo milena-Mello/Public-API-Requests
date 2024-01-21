b34a93083eb52a113a68a0ddc4008f7d3bc2b756
Milena Mello <milenamello625@gmail.com>
2024-01-21 13:54:30 +0300

    Get and display 12 random users

--- student-files/js/scripts.js
+++ student-files/js/scripts.js
@@ -1,8 +1,23 @@
 async function getStudentData() {
   const response = await fetch('https://randomuser.me/api/?results=12');
   const data = await response.json();
-  console.log(data);
-  return data;
+  displayStudentData(data);
 }
+
+function displayStudentData(data) {
+    const students = data.results;
+    let studentHTML = students.map(student => 
+        `<div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${student.picture.medium}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${student.name.first} ${student.name.last}</h3>
+            <p class="card-text">${student.email}</p>
+            <p class="card-text cap">${student.location.city}, ${student.location.state}</p>
+        </div>
+    </div>`).join('');
+    document.querySelector('.gallery').insertAdjacentHTML('beforeend', studentHTML);
+};
 
 getStudentData();
